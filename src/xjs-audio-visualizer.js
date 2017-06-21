ac52455faf8d8e2661e3133da7498eab48f88ea4
Erick Rodriguez <tbogard@outlook.com>
2017-06-21 10:20:05 -0400

    removed old comments from old visualizer

--- src/xjs-audio-visualizer.js
+++ src/xjs-audio-visualizer.js
@@ -239,7 +239,8 @@ var XBCAudioVisualizer = function(config = {}){
 			audioDeviceId : '',
 			hasCustomSoundAllowed : false,
 			customSoundAllowed : function() {},
-			customSoundNotAllowed : function() {}
+			customSoundNotAllowed : function() {},
+			is3d : false
 		}
 
 		/**
@@ -275,100 +276,5 @@ $(function(){
 		canvas_width : '100%',
 		canvas_height : '100%'
 	}
-	new XBCAudioVisualizer(config);
-	
+	new XBCAudioVisualizer(config);	
 })
-
-
-
-/*let paths = null;
-let visualizer = null;
-let mask = null;
-let path = null;
-let report = null;
-let audioContent = new AudioContext();
-
-let log = (logVal) => { 
-	getLog().innerHTML = logVal + '\n<br>'; 
-} 
-
-let  getLog = () => { 
-	return document.getElementById('log'); 
-} 
-
-let soundAllowed = (stream) => {;
-	window.persistAudioStream = stream;
-	let audioStream = audioContent.createMediaStreamSource( stream );
-	let analyser = audioContent.createAnalyser();
-	analyser.fftSize = 1024;
-	let frequencyArray = new Uint8Array(analyser.frequencyBinCount);
-	visualizer.setAttribute('viewBox', '0 0 500 500');
-	let javascriptNode = audioContent.createScriptProcessor(1024,1,1);
-	for (let i = 0 ; i < 255; i++) {
-		path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-		path.setAttribute('stroke-dasharray', '1,1');
-		path.setAttribute('stroke-width','0.5px');
-		mask.appendChild(path);
-	}
-
-	javascriptNode.onaudioprocess = (e) => {
-		analyser.getByteFrequencyData(frequencyArray);
-		let adjustedLength;
-		for (let i = 0 ; i < 255; i++) {
-			adjustedLength = Math.floor(frequencyArray[i]) - (Math.floor(frequencyArray[i]) % 5);
-			paths[i].setAttribute('d', 'M '+ (i) +',255 l 0,-' + adjustedLength);
-		}
- 
-	}
-	audioStream.connect(analyser); 
-	analyser.connect(javascriptNode); 
-	javascriptNode.connect(audioContent.destination); 
-}
-
-let soundNotAllowed = (error) => {
-	console.log(error);
-	console.warn("You must allow your microphone.");
-}
-
-let setSelectOptions = (audioSources) => {
-	let selectAudio = document.getElementById('selectAudio');
-	for (let i = audioSources.length - 1; i >= 0; i--) {
-		if (audioSources[i].kind === 'audioinput') {
-			let newOption = document.createElement('option');
-			newOption.text = audioSources[i].label;
-			newOption.value = audioSources[i].deviceId;
-			if(audioSources[i].label.indexOf('XSplitBroadcaster (DirectShow)') === 0){
-				newOption.setAttribute('selected','selected');
-			}
-			selectAudio.appendChild(newOption);
-		}
-	}
-}
-
-let setXBCAudioDeviceAsSource = (audioSources) => {
-	let XBCAudioDeviceId = null;
-	for (let i = audioSources.length - 1; i >= 0; i--) {
-		if (audioSources[i].kind === 'audioinput') {
-			if(audioSources[i].label.indexOf('XSplitBroadcaster (DirectShow)') === 0){
-				XBCAudioDeviceId = audioSources[i].deviceId;
-				break;
-			}
-		}
-	}
-	if(XBCAudioDeviceId === null){
-		throw 'no audio device was found';
-	}
-	navigator.getUserMedia({
-		video: false,
-		audio: {deviceId : {
-			exact: XBCAudioDeviceId
-		}}
-	}, soundAllowed, soundNotAllowed); 
-}*/
-
-/*window.onload = () => {
-	paths = document.getElementsByTagName('path');
-	visualizer = document.getElementById('visualizer');
-	mask = visualizer.getElementById('mask'); 
-	navigator.mediaDevices.enumerateDevices().then(setXBCAudioDeviceAsSource);
-};*/
