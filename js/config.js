256187f9d623ad915aa464f936287a13624d5b72
Erick Rodriguez <tbogard@outlook.com>
2017-06-27 03:16:34 -0400

    addding extra documentation

--- js/config.js
+++ js/config.js
@@ -1,11 +1,57 @@
 (function(){
 	'use strict'
-	var xjs = require('xjs');
-	var Item = xjs.Source;
-	var SourcePropsWindow = xjs.SourcePropsWindow;
-	var propsWindow = SourcePropsWindow.getInstance();
-	var currentSource;
-	xjs.ready().then(function() {
+
+	/**
+	 * [updateConfigGUI changes the config GUI to match the config]
+	 * @type {[type]}
+	 */
+	let updateConfigGUI = (config)=>{
+
+	};
+
+	/**
+	 * [updateConfig saves the configuration passed by the argument.]
+	 * @type {[type]}
+	 */
+	let updateConfig = (item)=>{
+		let config = {
+			skin : 'bars'
+		}
+		item.requestSaveConfig(config)
+	};
+
+	/**
+	 * [setGUILogic provide the GUI elements with actions to update the configuration of the view]
+	 * @return {[type]} [description]
+	 */
+	let setGUILogic = () =>{
+
+	}
+
+	/**
+	 * [xjs is the XJS Framework]
+	 * @type {object}
+	 */
+	let xjs = require('xjs');
+	/**
+	 * [Item holds the current source]
+	 * @type {Object}
+	 */
+	, Item = xjs.Source;
+	/**
+	 * [SourcePropsWindow Contains the properties of the source window]
+	 * @type {[type]}
+	 */
+	, SourcePropsWindow = xjs.SourcePropsWindow;
+	, propsWindow = SourcePropsWindow.getInstance();
+	, currentSource;
+
+	/**
+	 * [then we run the concatenated sets of promises to get and apply the config.]
+	 * @param  {Function} ).then(() [description]
+	 * @return {[type]}             [description]
+	 */
+	xjs.ready().then(() =>{
 		var configWindow =  SourcePropsWindow.getInstance();
 		propsWindow.useTabbedWindow({
 			customTabs: ['Audio Visualizer'],
@@ -16,7 +62,10 @@
 		currentSource = myItem;
 		return currentSource.loadConfig();
 	}).then((cfg)=>{
-		debugger;
-		console.log('config:',cfg)
+		console.log('config:',cfg);
+		setGUILogic();
+		//get the config to the GUI
+		updateConfigGUI(cfg);
+
 	});
 })();
