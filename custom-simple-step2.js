678baa86fe42a7cbbad5f93da88de279ed2d23ed
Erick Rodriguez <tbogard@outlook.com>
2017-07-02 17:10:31 -0400

    Small tidy clean up on the custom simple visualization example

--- custom-simple-step2.js
+++ custom-simple-step2.js
@@ -8,11 +8,9 @@ var analyser          = XBC_avz.analyser;
 var canvas            = XBC_avz.canvas;
 var ctx               = XBC_avz.visualizer;
 var mediaStreamSource = XBC_avz.mediaStreamSource;
-
 canvas.width          = window.innerWidth;
 canvas.height         = window.innerHeight;
 analyser.fftSize      = XBC_avz.fftsize;
-
 var bufferLength      = analyser.frequencyBinCount;
 var dataArray         = new Uint8Array(bufferLength);
 var WIDTH             = canvas.width;
@@ -22,22 +20,20 @@ var barHeight;
 var x                 = 0;
 
 function renderFrame() {
-  requestAnimationFrame(renderFrame);
-  x = 0;
-  analyser.getByteFrequencyData(dataArray);
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, WIDTH, HEIGHT);
-  for (var i = 0; i < bufferLength; i++) {
-    barHeight = dataArray[i];
-    var r = barHeight + (25 * (i/bufferLength));
-    var g = 250 * (i/bufferLength);
-    var b = 50;
-
-    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-    ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
-    x += barWidth + 1;
-  }
+    requestAnimationFrame(renderFrame);
+    x = 0;
+    analyser.getByteFrequencyData(dataArray);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        var r = barHeight + (25 * (i/bufferLength));
+        var g = 250 * (i/bufferLength);
+        var b = 50;
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+        x += barWidth + 1;
+    }
 }
 renderFrame();
 mediaStreamSource.connect(analyser);
